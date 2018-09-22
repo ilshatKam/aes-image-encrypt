41f76d334ce344aa36f36916efd12819558f00ff
--replace-all <-replace-all>
2018-09-23 02:58:18 +0500

    get from massive realised

--- test_image.py
+++ test_image.py
@@ -10,18 +10,25 @@ key = bytes("This_key_for_demo_purposes_only!", 'utf-8')
 iv = "InitializationVe"
 print('len ',len(key))
 aes = pyaes.AESModeOfOperationECB(key)
-plaintext = "TextMustBe16Byte"
 
-ciphertext = aes.encrypt(plaintext)
+input = [255, 255, 255, 255, 5, 6, 7, 7, 7, 7, 11, 12, 13, 254, 15, 240]
+plaintext = [0, 0, 0, 0]
+i = 0
+max = (len(input)//16)
+for i in range(0, max):
+    plaintext[0] = (input[16*i] << 24) | (input[16*i+1] << 16) | (input[16*i+2] << 8) | input[16*i+3]
+    for j in range(1, 4): 
+        plaintext[j] = (input[4*j+16*i] << 24) | (input[4*j+16*i+1] << 16) | (input[4*j+16*i+2] << 8) | input[4*j+16*i+3]
+    ciphertext = aes.encrypt(plaintext)
 
 # 'L6\x95\x85\xe4\xd9\xf1\x8a\xfb\xe5\x94X\x80|\x19\xc3'
-print (repr(ciphertext))
+    print (repr(ciphertext))
 
 # Since there is no state stored in this mode of operation, it
 # is not necessary to create a new aes object for decryption.
 #aes = pyaes.AESModeOfOperationECB(key)
-decrypted = aes.decrypt(ciphertext)
+    decrypted = aes.decrypt(ciphertext)
 
 # True
-print ('decrypted ', decrypted)
-print (decrypted == plaintext)
+    print ('decrypted ', decrypted)
+    print (decrypted == plaintext)
