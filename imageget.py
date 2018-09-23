10070e978ee553fdb0da4b5b54634696ce86f640
--replace-all <-replace-all>
2018-09-23 13:13:59 +0500

    working

--- imageget.py
+++ imageget.py
@@ -1,5 +1,13 @@
 from PIL import Image
-im = Image.open("E:\Work\Report\Python\ilshat\honda.jpg")
-im.show()
+import test_image
+in_img = Image.open("E:\Work\Report\Python\ilshat\kot.bmp")
 
-print(list(im.getdata()))
+in_arr=list(in_img.getdata())
+#print(in_arr)
+
+out_arr = test_image.encrypt(in_arr)
+print(out_arr)
+out_img = Image.new(in_img.mode, in_img.size)
+out_img.putdata(out_arr)
+out_img.save('kot_out.bmp')
+out_img.show()
--- test_image.py
+++ test_image.py
@@ -1,33 +1,40 @@
 import pyaes
 import os
 import sys
-
-# A 256 bit (32 byte) key
-key = bytes("This_key_for_demo_purposes_only!", 'utf-8') 
-
-# For some modes of operation we need a random initialization vector
-# of 16 bytes
-iv = "InitializationVe"
-print('len ',len(key))
-aes = pyaes.AESModeOfOperationECB(key)
-
-input = [255, 255, 255, 255, 5, 6, 7, 7, 7, 7, 11, 12, 13, 254, 15, 240]
-plaintext = [0, 0, 0, 0]
-i = 0
-max = (len(input)//16)
-for i in range(0, max):
-    plaintext[0] = (input[16*i] << 24) | (input[16*i+1] << 16) | (input[16*i+2] << 8) | input[16*i+3]
-    for j in range(1, 4): 
-        plaintext[j] = (input[4*j+16*i] << 24) | (input[4*j+16*i+1] << 16) | (input[4*j+16*i+2] << 8) | input[4*j+16*i+3]
-    ciphertext = aes.encrypt(plaintext)
-
+def encrypt(input):
+    # A 256 bit (32 byte) key
+    key = bytes("This_key_for_demo_purposes_only!", 'utf-8') 
+    
+    # For some modes of operation we need a random initialization vector
+    # of 16 bytes
+    iv = "InitializationVe"
+    print('len ',len(key))
+    aes = pyaes.AESModeOfOperationECB(key)
+    
+    #input = [255, 255, 255, 255, 5, 6, 7, 7, 7, 7, 11, 12, 13, 254, 15, 240]
+    plaintext = [0, 0, 0, 0]
+    i = 0
+    max = (len(input)//16)
+    ciphertext_arr = [ ]
+    for i in range(0, max):
+        plaintext[0] = (input[16*i] << 24) | (input[16*i+1] << 16) | (input[16*i+2] << 8) | input[16*i+3]
+        for j in range(1, 4): 
+            plaintext[j] = (input[4*j+16*i] << 24) | (input[4*j+16*i+1] << 16) | (input[4*j+16*i+2] << 8) | input[4*j+16*i+3]
+        ciphertext = aes.encrypt(plaintext)
+        for m in range(0, 16):
+        	ciphertext_arr.append(ciphertext[m])
+        #ciphertext_arr = [ struct.unpack('>i', ciphertext[i:i + 4])[0] for i in xrange(0, len(key), 4) ]
+        print('len ciphertext', len(ciphertext))
+       # decrypted = [aes.decrypt(ciphertext(i))]
+    return ciphertext_arr
+    
 # 'L6\x95\x85\xe4\xd9\xf1\x8a\xfb\xe5\x94X\x80|\x19\xc3'
     print (repr(ciphertext))
 
 # Since there is no state stored in this mode of operation, it
 # is not necessary to create a new aes object for decryption.
 #aes = pyaes.AESModeOfOperationECB(key)
-    decrypted = aes.decrypt(ciphertext)
+    
 
 # True
     print ('decrypted ', decrypted)
